328356a4a737c30ef308c91988888010e909aed9
Greg Weaver <gweaver187@gmail.com>
2019-08-15 21:54:32 -0400

    final2.0

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1.
-* 2.
-* 3.
-* 4.
+* 1.The value of 'this' will equal the window(console) where you are running the program*
+* 2.When proceeded by a dot, the object to the left gets 'this'
+* 3.When using a constructor function, 'this' equals the new object that is created and returned
+* 4.When call, bind, or apply methods are used, 'this' is directly defined
 *
 * write out a code example of each explanation above
 */
